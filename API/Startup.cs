dde8b047656bc9334726df0d6b7a5632369f5686
Waleed Ahmed <waleedahmed@192-168-1-104.tpgi.com.au>
2020-07-11 18:47:11 +1000

    Added Reactivities TypeScript project

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString
                 ("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -44,6 +49,8 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
